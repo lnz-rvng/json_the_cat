db3c9d462458f5ac4f50c270208e3a60e73646fa
Lance <lanceirvingmanzanal@icloud.com>
2024-01-25 20:23:47 -0700

    added a function that fetch data to an API

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,30 +1,27 @@
 const request = require('request');
 
-const breedFetcher = (breedName) => {
+const fetchBreedDescription = (breedName, cb) => {
 
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
   request(url, (error, response, body) => {
     if (error) {
-      console.log(`Request failed. ${error}`);
+      // call the callback if there's an error
+      cb(error.message, null);
       return;
     }
-
+    
     const data = JSON.parse(body);
     
     if (data.length > 0) {
-      console.log(data[0].description);
+      // call the callback if condition is true
+      cb(null, data[0].description);
     } else {
-      console.log(`${breedName} not found.`);
+      cb(`${breedName} not found.`, null);
     }
   });
 };
 
-const args = process.argv.slice(2);
-const breedName = args[0];
 
-if (!breedName) {
-  console.log(`Please provide a breed name in the CLI`);
-} else {
-  breedFetcher(breedName);
-}
+
+module.exports = { fetchBreedDescription };
